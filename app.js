f14dc784753a5eef9ffe29fbc933172f92a0170c
Darkos0912 <villanuevadarkos@gmail.com>
2023-08-08 17:08:40 -0300

    Mercado Liebre Terminado .

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 3000
+const port = process.env.PORT || 3000;
 
-app.listen(PORT,()=> console.log("Servidor corriendo"));
+app.listen(port,()=> console.log(`Servidor corriendo en el puerto ${port}`));
 
 app.get("/home",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
